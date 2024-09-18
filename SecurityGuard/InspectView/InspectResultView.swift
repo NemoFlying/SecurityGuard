afddfdec16fe9979659ede999c8880ec39ee65b9
NemoFlying <895190626@qq.com>
2024-09-18 18:52:16 +0800

    V1版本

---
+++ SecurityGuard/InspectView/InspectResultView.swift
@@ -0,0 +1,18 @@
+//
+//  InspectResultView.swift
+//  SecurityGuard
+//
+//  Created by mac on 2024/9/17.
+//
+
+import SwiftUI
+
+struct InspectResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InspectResultView()
+}
